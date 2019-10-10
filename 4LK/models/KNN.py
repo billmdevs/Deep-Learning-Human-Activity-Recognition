be4366728cd57c4594609797f00774e8f3fe5913
Bill Morrisson <billmorrissonjr@gmail.com>
2019-10-10 15:36:57 -0400

    Starting code provided by prof: 4 models and cifar dataset

---
+++ 4LK/models/KNN.py
@@ -0,0 +1,47 @@
+import numpy as np
+import scipy
+
+class KNN():
+    def __init__(self, k):
+        """
+        Initializes the KNN classifier with the k.
+        """
+        self.k = k
+    
+    def train(self, X, y):
+        """
+        Train the classifier. For k-nearest neighbors this is just 
+        memorizing the training data.
+
+        Inputs:
+        - X: A numpy array of shape (num_train, D) containing the training data
+          consisting of num_train samples each of dimension D.
+        - y: A numpy array of shape (N,) containing the training labels, where
+             y[i] is the label for X[i].
+        """
+    
+    def find_dist(self, X_test):
+        """
+        Compute the distance between each test point in X and each training point
+        in self.X_train.
+
+        Hint : Use scipy.spatial.distance.cdist
+
+        Returns :
+        - dist_ : Distances between each test point and training point
+        """
+        return dist_
+    
+    def predict(self, X_test):
+        """
+        Predict labels for test data using the computed distances.
+
+        Inputs:
+        - X_test: A numpy array of shape (num_test, D) containing test data consisting
+             of num_test samples each of dimension D.
+
+        Returns:
+        - pred: A numpy array of shape (num_test,) containing predicted labels for the
+          test data, where y[i] is the predicted label for the test point X[i].  
+        """
+        return pred
---
+++ 4LK/models/LinearRegression.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class LinearRegression():
+    def __init__(self):
+        """
+        Initialises Softmax classifier with initializing 
+        weights, alpha(learning rate), number of epochs
+        and regularization constant.
+        """
+        self.w = None
+        self.alpha = 0.5
+        self.epochs = 100
+        self.reg_const = 0.05
+    
+    
+    def train(self, X_train, y_train):
+        """
+        Train Linear regression classifier using function from Pytorch
+        """
+
+    
+    def predict(self, X_test):
+        """
+        Use the trained weights of softmax classifier to predict labels for
+        data points.
+
+        Inputs:
+        - X_test: A numpy array of shape (N, D) containing training data; there are N
+          training samples each of dimension D.
+
+        Returns:
+        - pred: Predicted labels for the data in X_test. pred is a 1-dimensional
+          array of length N, and each element is an integer giving the predicted
+          class.
+        """
+        return pred 
